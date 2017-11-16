2f8b76f96bdaf2ffda6f1120a83fa31a7e5ef743
Tanswer <Tanswer@localhost.localdomain>
2017-11-17 00:44:55 +0800

    RIO

--- rio.c
+++ rio.c
@@ -102,8 +102,48 @@ ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
 }
 
 
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
+{
+    int i,n;
+    char c,*bufp = usrbuf;
+    for(i = 1; i<maxlen; i++){
+        if((n = rio_read(rp, &c, 1)) == 1){     //每次读取一个字符
+            *bufp++ = c;
+            if(c == '\n')
+                break;
+        } else if(n == 0){
+            if(i == 1)      //空文件
+                return 0;
+            else
+                break;      //读到部分数据
+        } else
+            return -1;      //出错
+    }
+
+    *bufp = 0;
+    return i;
+}
+
+
 //带缓冲输入函数
 ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n)
 {
-    
+    size_t nleft = n;
+    ssize_t nread;
+    char *bufp = usrbuf;
+
+    while(nleft > 0){
+        if((nread = rio_read(rp, bufp, nleft)) < 0){
+            if(errno == EINTR)
+                nread = 0;
+            else
+                return -1;      
+        } else if( nread == 0 )
+            break;      //EOF
+        
+        nleft -= nread;
+        bufp += nread;
+    }
+
+    return (n - nleft);
 }
